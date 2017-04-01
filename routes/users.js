bf7bb9fea46d63c050613be411f725308ba689ee
Vitor Santo <Vitor Santo>
2017-04-01 16:54:45 -0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+router.get('/usernome/:nome', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToShow = req.params.nome;
+    collection.find({ 'nome' : userToShow }, function(e,docs){
+         res.json(docs);
+    });
+});
+
+
+/*
+ * POST 
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE 
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;
