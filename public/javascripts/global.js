252b8eb953eb9518832df15c8658133215f86f9f
Vitor Santo <vitor.gomessanto@gmail.com>
2017-04-01 17:41:59 -0300

    ajax de delecao e criacao de produtos e populacao da lista de produtos

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,11 +1,14 @@
-// Userlist data array for filling in info box
+// Userlist data array for filling in user tab
 var userListData = [];
+// Product data array for filling in product tab
+var prodListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
 
     // Populate the user table on initial page load
     populateTable();
+    populateProdTable();
 
     // Username link click
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
@@ -16,11 +19,21 @@ $(document).ready(function() {
     // Delete User link click
     $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 
+
+    // Product link click
+     $('#productList table tbody').on('click', 'td a.linkshowuser', showProductInfo);
+
+    // Add Product button click
+    $('#btnAddProduct').on('click', addProduct);
+
+    // Delete Product link click
+    $('#productList table tbody').on('click', 'td a.linkdeleteuser', deleteProduct);
+
 });
 
 // Functions =============================================================
 
-// Fill table with data
+// Fill user table with data
 function populateTable() {
 
     // Empty content string
@@ -46,6 +59,33 @@ function populateTable() {
     });
 };
 
+// Fill product tab with data
+function populateProdTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/product/productList', function( data ) {
+
+        // Stick our user data array into a userlist variable in the global object
+        prodListData = data;
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowprod" rel="' + this.nome + '" title="Detalhes">' + this.nome + '</a></td>';
+            tableContent += '<td>' + this.preco + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteprod" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#prodList table tbody').html(tableContent);
+    });
+};
+
+
 // Show User Info
 function showUserInfo(event) {
 
@@ -65,6 +105,28 @@ function showUserInfo(event) {
     $('#userInfoLocation').text(thisUserObject.endereco);
 	$('#userInfoName').text(thisUserObject.nome);
 	$('#userInfoEmail').text(thisUserObject.email);
+
+};
+
+
+// Show Product Info
+function showProductInfo(event) {
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    var thisProdName = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    var arrayPosition = prodListData.map(function(arrayItem) { return arrayItem.nome; }).indexOf(thisProdName);
+
+    // Get our User Object
+    var thisProdObject = prodListData[arrayPosition];
+
+    //Populate Info Box
+	$('#prodInfoName').text(thisProdObject.nome);
+	$('#prodInfoPrice').text(thisProdObject.preco);
 
 };
 
@@ -121,6 +183,59 @@ function addUser(event) {
     }
 };
 
+// Add Product
+function addProduct(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addProduct input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var produto = {
+            'nome': $('#addProd fieldset input#inputProdName').val(),
+            'preco': $('#addProd fieldset input#inputProdEmail').val()
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: produto,
+            url: '/product/addproduct',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addProd fieldset input').val('');
+
+                // Update the table
+                populateProdTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Por favor preencha todos os campos.');
+        return false;
+    }
+};
+
+
 // Delete User
 function deleteUser(event) {
 
@@ -147,6 +262,45 @@ function deleteUser(event) {
 
             // Update the table
             populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
+
+// Delete User
+function deleteProduct(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Tem certeza que deseja excluir?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/product/deleteproduct/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateProdTable();
 
         });
 
